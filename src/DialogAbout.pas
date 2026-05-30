ed4e85bd89da70a3fbb02e5781e78703edd03579
Jaisal E. K. <mail@jaisal.in>
2026-05-30 07:30:59 +0000

    Style: Reorder methods in DialogAbout.pas for sequential readability

--- src/DialogAbout.pas
+++ src/DialogAbout.pas
@@ -1,16 +1,16 @@
 {
  Copyright © 2026 Jaisal E. K.
- 
+
  This program is free software: you can redistribute it and/or modify it
  under the terms of the GNU Affero General Public License as published
  by the Free Software Foundation, either version 3 of the License, or
  (at your option) any later version.
- 
+
  This program is distributed in the hope that it will be useful,
  but WITHOUT ANY WARRANTY; without even the implied warranty of
  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
  GNU Affero General Public License for more details.
- 
+
  You should have received a copy of the GNU Affero General Public License
  along with this program. If not, see <https://www.gnu.org/licenses/>.
 }
@@ -141,6 +141,18 @@ begin
   HideCaret(TMemo(Sender).Handle);
 end;
 
+procedure TfrmDialogAbout.pmnDialogAboutMemoPopup(Sender: TObject);
+var
+  TargetMemo: TMemo;
+begin
+  if (pmnDialogAboutMemo.PopupComponent is TMemo) then
+  begin
+    TargetMemo := TMemo(pmnDialogAboutMemo.PopupComponent);
+    mniDialogAboutMemoCopy.Enabled := TargetMemo.SelLength > 0;
+    mniDialogAboutMemoSelectAll.Enabled := Length(TargetMemo.Text) > 0;
+  end;
+end;
+
 procedure TfrmDialogAbout.mniDialogAboutMemoCopyClick(Sender: TObject);
 var
   TargetMemo: TMemo;
@@ -162,18 +174,6 @@ begin
     TargetMemo := TMemo(pmnDialogAboutMemo.PopupComponent);
     if TargetMemo.CanFocus then TargetMemo.SetFocus;
     TargetMemo.SelectAll;
-  end;
-end;
-
-procedure TfrmDialogAbout.pmnDialogAboutMemoPopup(Sender: TObject);
-var
-  TargetMemo: TMemo;
-begin
-  if (pmnDialogAboutMemo.PopupComponent is TMemo) then
-  begin
-    TargetMemo := TMemo(pmnDialogAboutMemo.PopupComponent);
-    mniDialogAboutMemoCopy.Enabled := TargetMemo.SelLength > 0;
-    mniDialogAboutMemoSelectAll.Enabled := Length(TargetMemo.Text) > 0;
   end;
 end;
 
